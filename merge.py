5601ba9c1c938f0f2eba5c27afab107dce13cc9c
nebolax <alexey.nebolsin.jr@gmail.com>
2021-12-04 11:02:29 +0300

    update merge

--- merge.py
+++ merge.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 entire: pd.DataFrame = None
 for i in range(10):
-    path = f'scrapped/out{i}.csv'
+    path = f'scrap-kamtoday/out{i}.csv'
     df = pd.read_csv(path, index_col='news_id')
     if i == 0:
         entire = df
     else:
         entire = entire.append(df)
-entire.to_csv('entire.csv')
+entire.to_csv('entire-kamtoday.csv')
