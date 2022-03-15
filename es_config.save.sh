679e3f5a0facac80c3e92eb513f3ec7d3c6e3aab
nebolax <alexey.nebolsin.jr@gmail.com>
2022-03-15 14:12:51 +0400

    removed es_config.save

--- es_config.save.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# import elasticseacrch PGP key
-eval $"wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -"
-# install apt-transport-https package
-eval $"sudo apt-get install apt-transport-https"
-# add elasticsearch repo
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
